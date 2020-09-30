330bc998ce11fdda34b7a96503f768f9bc972625
Michael Yuen <iam@mikey.baby>
2020-09-30 00:38:26 -0400

    Create app and add code to reproduce issue

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { github } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+function App() {
+  const codeString = `
+  <script type="application/ld+json">
+  {
+      "@context": "https://schema.org/",
+      "@type": "FAQPage",
+      "mainEntity": [{
+        "@type": "Question",
+        "name": "",
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": ""
+        }
+      }]
+    }
+  </script>`;
+  return (
+    <div
+      style={{
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "100%",
+      }}
+    >
+      <h1>React Syntax Highlighter Issue Reproduction</h1>
+      <SyntaxHighlighter language="javascript" style={github}>
+        {codeString}
+      </SyntaxHighlighter>
+    </div>
+  );
+}
+
+export default App;
