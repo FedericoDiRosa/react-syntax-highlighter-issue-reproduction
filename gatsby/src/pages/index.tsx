21ca77497cb0d7f676006903fa4498053dbb5e72
Federico Di Rosa <federico.dirosa88@gmail.com>
2020-10-05 09:48:23 +0100

    Use PrismLight build

--- gatsby/src/pages/index.tsx
+++ gatsby/src/pages/index.tsx
@@ -1,24 +1,24 @@
 import React from "react"
-import SyntaxHighlighter from "react-syntax-highlighter"
-import { github } from "react-syntax-highlighter/dist/esm/styles/hljs"
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter"
+import bash from "react-syntax-highlighter/dist/esm/languages/prism/bash"
+import javascript from "react-syntax-highlighter/dist/esm/languages/prism/javascript"
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism"
 import "./index.css"
 
+SyntaxHighlighter.registerLanguage("bash", bash)
+SyntaxHighlighter.registerLanguage("javascript", javascript)
+
 export default function Home() {
-  const codeString = `
-  <script type="application/ld+json">
-  {
-      "@context": "https://schema.org/",
-      "@type": "FAQPage",
-      "mainEntity": [{
-        "@type": "Question",
-        "name": "",
-        "acceptedAnswer": {
-          "@type": "Answer",
-          "text": ""
-        }
-      }]
-    }
-  </script>`
+  const codeString = `export const retryOnFail = async (url, callback, n) => {
+  try {
+    const { data } = await Axios.get(url)
+    callback(data)
+    return data
+  } catch (error) {
+    if (n === 1) console.log(error)
+    else setTimeout(() => retryOnFail(n - 1), 1000)
+  }
+}`
   return (
     <div
       style={{
@@ -33,14 +33,14 @@ export default function Home() {
       <h2>Gatsby</h2>
       <div>
         <div>created with:</div>
-        <SyntaxHighlighter language="bash" style={github}>
+        <SyntaxHighlighter language="bash" style={dracula}>
           gatsby new gatsby
           https://github.com/gatsbyjs/gatsby-starter-hello-world
         </SyntaxHighlighter>
       </div>
 
       <h3>Example</h3>
-      <SyntaxHighlighter language="javascript" style={github}>
+      <SyntaxHighlighter language="javascript" style={dracula}>
         {codeString}
       </SyntaxHighlighter>
     </div>
